6e06dc6d6ce16ce42b53aaf4ab186f71122b3fa9
CePersico~ <you@example.com>
2022-11-14 19:15:06 +0100

    orden parametros

---
+++ Funciones/orden_parametros.py
@@ -0,0 +1,18 @@
+
+
+# def parametros_ordenados(nombre, apellido, *args, **kwargs):
+#     pass  # ponemos esta sentencia para que no marque error al no tener ningun contenido la funcion
+
+
+
+
+# def parametros_desordenados(nombre, apellido, **kwargs, *args):
+
+    # pass  # ponemos esta sentencia para que no marque error al no tener ningun contenido la funcion
+
+#  cuando se ejecuta la funcion tira error                                                        
+#    ^
+# SyntaxError: invalid syntax
+
+# se debe mantener el orden establecido por Python:
+# parametros, *args y **kwargs
