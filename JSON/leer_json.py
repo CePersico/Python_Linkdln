2cbaf2ff38dc699366fe9dc5ebe161b45043dd80
CePersico~ <you@example.com>
2022-11-14 22:48:36 +0100

    leer JSON

---
+++ JSON/leer_json.py
@@ -0,0 +1,15 @@
+import json
+
+# with open("persona.json", "r") as archivo_json:
+#     datos_json = json.load(archivo_json)
+
+# print(type(datos_json))
+# print(datos_json)
+# print(datos_json["nombre"])
+
+with open("ejemplo.json", "r") as archivo_json:
+    datos_json = json.load(archivo_json)
+
+print(type(datos_json))
+# print(datos_json)
+print(datos_json["personas"])
